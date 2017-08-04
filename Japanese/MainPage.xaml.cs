09f3cce3405b6e051848b2179b80dfa7a9d210f1
PHPierre <meganium.151@gmail.com>
2017-08-04 14:53:26 +0200

    First commit

---
+++ Japanese/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Japanese
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Menu_Click(Object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void IconsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (HiraganaListBoxItem.IsSelected) { mainFrame.Navigate(typeof(PageHiragana)); mainTitle.Text = "Liste des Hiragana"; }
+            else if (KatanaListBoxItem.IsSelected) { mainFrame.Navigate(typeof(PageKatana)); mainTitle.Text = "Liste des Katana"; }
+            else if (KanjiListBoxItem.IsSelected) { mainFrame.Navigate(typeof(PageKanji)); mainTitle.Text = "Liste des Kanji"; }
+            /*
+             * var itemId = ((SampleDataItem)e.ClickedItem).UniqueId;
+             * this.Frame.Navigate(typeof(ItemPage), itemId);
+             */
+        }
+    }
+
+}
---
+++ Japanese/PageHiragana.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Japanese
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class PageHiragana : Page
+    {
+        public PageHiragana()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Character_Click(object sender, RoutedEventArgs e)
+        {
+            Button _button = (Button)sender;
+            String character = _button.Tag.ToString();
+
+            Dictionary<string, string> newDictionary = new Dictionary<string, string>();
+            newDictionary.Add("character", character);
+
+            this.Frame.Navigate(typeof(PageCharacter), newDictionary);
+        }
+
+    }
+}
